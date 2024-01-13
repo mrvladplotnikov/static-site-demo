7bb44b13fbaa5281f2025dd5f51705cab9297bd1
tarotum <onyskiv.maxim@gmail.com>
2024-01-13 16:57:02 +0200

    home page style update

--- themes/vp-static-theme/static/js/home.js
+++ themes/vp-static-theme/static/js/home.js
@@ -1,38 +1,37 @@
-'use strict';
-
 import Grid from "./shared/grid.js";
 
-new Grid(document.querySelector('.grid'), document.querySelectorAll('.grid-item'), 2).init({
-    '(max-width: 42em)': 2,
-    '(min-width: 42em)': 3,
-    '(min-width: 60em)': 4,
-    '(min-width: 81em)': 5,
-});
-
-
-const homePageNavMenu = () => {
-    const mobileMenu = document.querySelector("#mobile-menu-button");
-    const desktopMenu = document.querySelector("#horizontal-nav");
-
-    // desktopMenu hidden by default
-    desktopMenu.setAttribute('inert', true);
-
-    document.addEventListener("scrollend", () => {
-        const firstSectionHeight = window.innerHeight;
-        const width = window.innerWidth;
-        const scrolled = window.scrollY;
-
-        const isSeccondSection = scrolled > firstSectionHeight;
-        const isLargeScreen = width >= 1200;
-
-
-
-        mobileMenu.classList.toggle('hidden', isSeccondSection && isLargeScreen);
-
-        desktopMenu.classList.toggle('visible', isSeccondSection && isLargeScreen);
-        desktopMenu.toggleAttribute('inert', !isSeccondSection && isLargeScreen);
+(function () {
+    new Grid(document.querySelector('.grid'), document.querySelectorAll('.grid-item'), 2).init({
+        '(max-width: 42em)': 2,
+        '(min-width: 42em)': 3,
+        '(min-width: 60em)': 4,
+        '(min-width: 81em)': 5,
     });
-};
 
-homePageNavMenu();
 
+    function homePageNavMenu() {
+        const mobileMenu = document.querySelector("#mobile-menu-button");
+        const desktopMenu = document.querySelector("#horizontal-nav");
+
+        // desktopMenu hidden by default
+        desktopMenu.setAttribute('inert', true);
+
+        document.addEventListener("scrollend", () => {
+            const firstSectionHeight = window.innerHeight;
+            const width = window.innerWidth;
+            const scrolled = window.scrollY;
+
+            const isSeccondSection = scrolled > firstSectionHeight;
+            const isLargeScreen = width >= 1200;
+
+
+
+            mobileMenu.classList.toggle('hidden', isSeccondSection && isLargeScreen);
+
+            desktopMenu.classList.toggle('visible', isSeccondSection && isLargeScreen);
+            desktopMenu.toggleAttribute('inert', !isSeccondSection && isLargeScreen);
+        });
+    };
+
+    homePageNavMenu();
+})();
--- themes/vp-static-theme/static/js/portfolio.js
+++ themes/vp-static-theme/static/js/portfolio.js
@@ -1,123 +1,113 @@
-'use strict';
-
 import Grid from "./shared/grid.js";
 
-const urlKeys = {
-    category: "category",
-    service: "service",
-}
+(function () {
+    /**
+     * @param {NodeListOf<HTMLElement>} works 
+     * @param {string} category current category value
+     * @param {string} service current service value
+     * @returns {NodeListOf<HTMLElement>}
+     */
+    function filterWorks(works = [], category, service) {
+        return Array.from(works).reduce((acc, workEl) => {
+            const workCategories = workEl.dataset.categories || "";
+            const workServices = workEl.dataset.services || "";
+            const isSelected = workCategories.includes(category || "") && workServices.includes(service || "");
 
-/**
- * @param {NodeListOf<HTMLElement>} works 
- * @param {string} category 
- * @param {string} service
- * @returns {NodeListOf<HTMLElement>}
- */
-function filterWorks(works = [], category, service) {
-    return Array.from(works).reduce((acc, workEl) => {
-        const workCategories = workEl.dataset.categories || "";
-        const workServices = workEl.dataset.services || "";
-        const isSelected = workCategories.includes(category || "") && workServices.includes(service || "");
+            if (!isSelected) {
+                return acc;
+            }
 
-        if (isSelected) {
-            workEl.classList.remove("grid-item-hidden");
-        } else {
-            workEl.classList.add("grid-item-hidden");
-        }
-
-        if (isSelected) {
             return acc.concat(workEl);
-        }
-
-        return acc;
-    }, []);
-}
-
-// Intial Page Data
-const gridEl = document.querySelector('.grid');
-const gridItemsEl = document.querySelectorAll('.grid-item');
-const initialSearchParams = new URLSearchParams(window.location.search);
-const initialCategory = initialSearchParams.get(urlKeys.category) || "";
-const initialService = initialSearchParams.get(urlKeys.service) || "";
-
-const filteredIWorks = filterWorks(
-    gridItemsEl,
-    initialSearchParams.get(urlKeys.category),
-    initialSearchParams.get(urlKeys.service)
-);
-
-const grid = new Grid(gridEl, filteredIWorks, 2).init({
-    '(max-width: 42em)': 2,
-    '(min-width: 42em)': 3,
-    '(min-width: 60em)': 4,
-    '(min-width: 81em)': 5,
-});
-
-/**
- * @param {Record<string, string>} params 
- */
-function updateSelectedWorks(params = {}) {
-    const searchParams = new URLSearchParams(window.location.search);
-    let url = window.location.pathname;
-
-    Object.keys(params).forEach(key => {
-        const isEmptyValue = !params[key];
-
-        if (isEmptyValue) {
-            searchParams.delete(key);
-        } else {
-            searchParams.set(key, params[key]);
-        }
-    });
-
-    if (searchParams.size > 0) {
-        url = "?" + searchParams.toString();
+        }, []);
     }
 
-    history.pushState(null, "", url); // changing url 
+    const urlKeys = {
+        category: "category",
+        service: "service",
+    };
 
-    const updatedWorks = filterWorks(
-        gridItemsEl,
-        searchParams.get(urlKeys.category),
-        searchParams.get(urlKeys.service)
-    );
+    // Intial Page Data
+    const gridEl = document.querySelector('.grid');
+    const gridItemsEl = document.querySelectorAll('.grid-item');
+    const initialSearchParams = new URLSearchParams(window.location.search);
+    const initialCategory = initialSearchParams.get(urlKeys.category) || "";
+    const initialService = initialSearchParams.get(urlKeys.service) || "";
 
-    // re-generating grid of works
-    grid.updateGrid(updatedWorks);
-}
+    const filteredIWorks = filterWorks(gridItemsEl, initialCategory, initialService);
 
-const worksCategorySelectEl = document.querySelector("#works-category-select");
-const worksCategoryResetEl = document.querySelector("#works-category-reset");
-const worksServiceSelectEl = document.querySelector("#works-service-select");
-const worksServiceRestEl = document.querySelector("#works-service-reset");
-const worksRestEl = document.querySelector("#works-reset");
-
-if (worksCategorySelectEl && worksServiceSelectEl) {
-    worksCategorySelectEl.value = initialCategory;
-    worksCategorySelectEl.addEventListener('change', (event) => {
-        const category = event.target.value;
-        updateSelectedWorks({ category });
+    const grid = new Grid(gridEl, filteredIWorks, 2).init({
+        '(max-width: 42em)': 2,
+        '(min-width: 42em)': 3,
+        '(min-width: 60em)': 4,
+        '(min-width: 81em)': 5,
     });
 
-    worksCategoryResetEl.addEventListener('click', () => {
-        worksCategorySelectEl.value = "";
-        updateSelectedWorks({ category: "" });
-    });
+    /**
+     * @param {Record<string, string>} params 
+     */
+    function updateSelectedWorks(params = {}) {
+        const searchParams = new URLSearchParams(window.location.search);
+        let url = window.location.pathname;
 
-    worksServiceSelectEl.value = initialService;
-    worksServiceSelectEl.addEventListener('change', (event) => {
-        const service = event.target.value;
-        updateSelectedWorks({ service });
-    });
+        Object.keys(params).forEach(key => {
+            const isEmptyValue = !params[key];
 
-    worksServiceRestEl.addEventListener('click', () => {
-        worksServiceSelectEl.value = "";
-        updateSelectedWorks({ service: "" });
-    });
+            if (isEmptyValue) {
+                searchParams.delete(key);
+            } else {
+                searchParams.set(key, params[key]);
+            }
+        });
 
-    worksRestEl.addEventListener('click', () => {
-        worksCategorySelectEl.value = "";
-        worksServiceSelectEl.value = "";
-        updateSelectedWorks({ service: "", category: "" });
-    });
-}
+        if (searchParams.size > 0) {
+            url = "?" + searchParams.toString();
+        }
+
+        history.pushState(null, "", url); // changing url 
+
+        const updatedWorks = filterWorks(
+            gridItemsEl,
+            searchParams.get(urlKeys.category),
+            searchParams.get(urlKeys.service)
+        );
+
+        // re-generating grid of works
+        grid.updateGrid(updatedWorks);
+    }
+
+    const worksCategorySelectEl = document.querySelector("#works-category-select");
+    const worksCategoryResetEl = document.querySelector("#works-category-reset");
+    const worksServiceSelectEl = document.querySelector("#works-service-select");
+    const worksServiceRestEl = document.querySelector("#works-service-reset");
+    const worksRestEl = document.querySelector("#works-reset");
+
+    if (worksCategorySelectEl && worksServiceSelectEl) {
+        worksCategorySelectEl.value = initialCategory;
+        worksCategorySelectEl.addEventListener('change', (event) => {
+            const category = event.target.value;
+            updateSelectedWorks({ category });
+        });
+
+        worksCategoryResetEl.addEventListener('click', () => {
+            worksCategorySelectEl.value = "";
+            updateSelectedWorks({ category: "" });
+        });
+
+        worksServiceSelectEl.value = initialService;
+        worksServiceSelectEl.addEventListener('change', (event) => {
+            const service = event.target.value;
+            updateSelectedWorks({ service });
+        });
+
+        worksServiceRestEl.addEventListener('click', () => {
+            worksServiceSelectEl.value = "";
+            updateSelectedWorks({ service: "" });
+        });
+
+        worksRestEl.addEventListener('click', () => {
+            worksCategorySelectEl.value = "";
+            worksServiceSelectEl.value = "";
+            updateSelectedWorks({ service: "", category: "" });
+        });
+    }
+})();
